4ea6ce045bdad505830fa9aa0b0e3a96a3baecb0
Ted Barlow <ted@noukod.com>
2018-12-05 21:26:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+  #return badge
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |names|
+    badges = badge_maker(names)
+  end
+  return badges
+end
